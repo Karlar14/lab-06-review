bc59899764f7d807c33492722f692b5d73d53400
Karla Ramirez <Karla@DarknessSadness.cable.rcn.com>
2020-10-18 13:55:11 -0400

    finished labs

---
+++ temperature.py
@@ -0,0 +1,3 @@
+tempincelsius = input("Please enter a temperature in celsius")
+tempinfarenheit = (float(tempincelsius) * (9 / 5) + 32.0)
+print("The temperature is " + str(tempinfarenheit) + "°F" )
